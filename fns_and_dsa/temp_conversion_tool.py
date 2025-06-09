80d03267a9abbb9e49debaeef517581ca12aab83
Your Name <your-email@example.com>
2025-06-09 12:02:30 +0300

    Redo: complete working version of temp conversion tool

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
-FAHRENHEIT_TO_CELSIUS_FACTOR = 5 / 9
-CELSIUS_TO_FAHRENHEIT_FACTOR = 9 / 5
+FAHRENHEIT_TO_CELSIUS_FACTOR = 5/9
+CELSIUS_TO_FAHRENHEIT_FACTOR = 9/5
 
 def convert_to_celsius(fahrenheit):
     """Converts Fahrenheit to Celsius using global factor"""
