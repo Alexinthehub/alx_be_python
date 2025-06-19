4e3dc9b9afd0834dd2c2b2c001735c9aa9ec182d
Your Name <your-email@example.com>
2025-06-20 02:09:38 +0300

    Fix: Rename test_multiply to test_multiplication as expected by ALX

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -16,12 +16,13 @@ class TestSimpleCalculator(unittest.TestCase):
         self.assertEqual(self.calc.subtract(3, 5), -2)
         self.assertEqual(self.calc.subtract(0, 0), 0)
 
-    def test_multiply(self):
-        self.assertEqual(self.calc.multiply(3, 4), 12)
-        self.assertEqual(self.calc.multiply(-2, 3), -6)
-        self.assertEqual(self.calc.multiply(0, 5), 0)
+def test_multiplication(self):
+    self.assertEqual(self.calc.multiply(3, 4), 12)
+    self.assertEqual(self.calc.multiply(-2, 3), -6)
+    self.assertEqual(self.calc.multiply(0, 5), 0)
 
-    def test_divide(self):
+
+    def test_division(self):
         self.assertEqual(self.calc.divide(10, 2), 5)
         self.assertAlmostEqual(self.calc.divide(7, 2), 3.5)
         self.assertEqual(self.calc.divide(-6, 3), -2)
